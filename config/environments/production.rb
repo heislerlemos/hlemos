4d569d78e141d3d80b236856c10ba779c7472c0e
Heisler <heisler.lemos@ita.co.ao>
2019-07-16 11:45:33 +0100

    updated production asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,10 +52,12 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
-
+  
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  config.serve_static_assets = true
+  config.assets.compile = true
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
