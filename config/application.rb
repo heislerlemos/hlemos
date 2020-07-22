c255c0ae720795a6ff55a2baefbf0e257714aae5
Heisler Lemos <heisler.lemos@gmail.com>
2020-07-22 14:22:30 +0100

    fightining

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
+#Coffee::Rails::TemplateHandler.call(template, source)
+
+
 require_relative 'boot'
 
 require 'rails/all'
+
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
