1d9782ddccddef1c2a9f516c897c04cb74f1a113
Heisler Lemos <heisler.stlano@gmail.com>
2020-09-24 14:33:15 +0100

    changed covid page

--- app/controllers/virus_controller.rb
+++ app/controllers/virus_controller.rb
@@ -1,16 +1,56 @@
 class VirusController < ApplicationController
   def index
   	   response = HTTParty.get('http://api.thevirustracker.com')
-    # Teste access 
 
     puts "At the moment covid server is #{response.code}"
-    
+
     if response.code == 200
 
-     @statistics = Covid19::GlobalStatistics.retrieve
-     @angolastat =  Covid19::GlobalStatistics.by(country: 'ao')
+     statistics = Covid19::GlobalStatistics.retrieve
+     @totalcases = JSON.parse(statistics)["results"].first["total_cases"]
+     @totalrecoverd = JSON.parse(statistics)["results"].first["total_recovered"]
+     @totaldeaths = JSON.parse(statistics)["results"].first["total_deaths"]
+ # Nacional
+     angolaestatica = Covid19::GlobalStatistics.by(country: 'ao')
+     @totalcasesangola  = JSON.parse(angolaestatica)["countrydata"].first["total_cases"]
+     @totalrecoverdangola = JSON.parse(angolaestatica)["countrydata"].first["total_recovered"]
+     @totaldeathsangola =  JSON.parse(angolaestatica)["countrydata"].first["total_deaths"]
+   @news = Covid19::News.retrieve
+     puts statistics.class
 
-   else 
+     portugalcovid =  Covid19::GlobalStatistics.by(country: 'pt')
+     @portugalestado = JSON.parse(portugalcovid)["countrydata"].first["total_cases"]
+     @portugalmortes = JSON.parse(portugalcovid)["countrydata"].first["total_deaths"]
+     @portugarecuperados = JSON.parse(portugalcovid)["countrydata"].first["total_recovered"]
+
+
+
+     brazilcovid =  Covid19::GlobalStatistics.by(country: 'br')
+     @brazilestado = JSON.parse( brazilcovid )["countrydata"].first["total_cases"]
+     @brazilmorte = JSON.parse( brazilcovid )["countrydata"].first["total_deaths"]
+     @brazilrecuperados = JSON.parse( brazilcovid )["countrydata"].first["total_recovered"]
+
+
+# Para verificar a info
+puts "---------------------------------"
+puts "---------------------------------"
+    puts "valores do mundo #{statistics}"
+    puts "---------------------------------"
+    puts "---------------------------------"
+
+    puts "valores de angola #{angolaestatica}"
+    puts "---------------------------------"
+    puts "---------------------------------"
+
+    puts "valores de portugal #{portugalcovid }"
+    puts "---------------------------------"
+    puts "---------------------------------"
+
+    puts "valores do brazil #{brazilcovid}"
+    puts "---------------------------------"
+    puts "---------------------------------"
+
+  else
     puts "no info covid at the moment" , notice: "At the moment we cannot have covid 19 info please try again later ."
   end
   end
