9c5e7885283d68bb15fba692049d42ea88561fcd
Heisler Lemos <heisler.stlano@gmail.com>
2020-10-29 11:33:50 +0100

    adding link images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
+  
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
