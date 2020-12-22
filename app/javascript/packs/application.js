ee15bbad7a4534b62e612146b487409e3762ef7b
Heisler Lemos <heisler.stlano@gmail.com>
2020-12-22 09:57:45 +0100

    added particles js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,11 @@ require("trix")
 require("@rails/actiontext")
 require("@rails/ujs").start()
 require("turbolinks").start()
+
+// Stimulus setup.
+import { Application } from 'stimulus'
+const application = Application.start()
+
+// Register the rails-particles.js controller.
+import Particles from 'rails-particles.js'
+application.register('particles', Particles)
