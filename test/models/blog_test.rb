066d3f90d5cb6d7ab759660e2ed79c7dcb83f58e
Heisler Lemos <heisler.stlano@gmail.com>
2021-01-23 11:59:40 +0100

    stating to create tests

--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -1,7 +1,30 @@
+# coding: utf-8
 require 'test_helper'
 
 class BlogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   #test "the truth" do
+    # assert 
+   #end
+
+
+
+   test "não deve gravar o blog sem o titulo" do
+     blog = Blog.new
+     blog.titulo  = "este é um titulo"
+     assert  blog.save, "não guardou sem titulo artigo em titulo"
+     
+   end
+
+
+
+   test "o titulo deve ter pelo 5 tamanho" do
+     blog = Blog.new
+
+      blog.titulo = "heisler"
+
+       assert blog.save, "não guardou com um nomero  menos que 5" 
+   
+     
+   end
+
 end
