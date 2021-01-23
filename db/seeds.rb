54de882f4dc00e3270efd0a41d61dee3b54997ff
Heisler Lemos <heisler.stlano@gmail.com>
2021-01-23 12:46:38 +0100

    change text to p and added seeds create default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+
+50.times do
+  blogs = Blog.create(titulo: "familia", texto: "está é a minha familia", image_url: "https://static01.nyt.com/images/2020/04/15/business/15Techfix-illo/15Techfix-illo-jumbo.gif?quality=90&auto=webp")
+end
