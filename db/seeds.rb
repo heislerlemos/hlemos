2b6876b44917a5f054560d39d08caf440c572222
heislerlemos <heisler.stlano@gmail.com>
2021-03-25 20:23:22 +0100

    add 12 sizes on pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 # coding: utf-8
 
 
-10.times do
+40.times do
   blogs = Blog.create(titulo: "familia", texto: "está é a minha familia", image_url: "https://static01.nyt.com/images/2020/04/15/business/15Techfix-illo/15Techfix-illo-jumbo.gif?quality=90&auto=webp")
    
 end
 
 
 
-10.times do
+40.times do
 
   blogs = Blog.create(titulo: "Musica", texto: "isto é lofi music", image_url: "https://i0.wp.com/historiadorgeek.com.br/wp-content/uploads/2020/08/Conhe%C3%A7a-o-Brasil-Lofi-Hip-Hop.jpg?w=715&ssl=1")
 
