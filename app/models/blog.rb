f63db553864deaa5c955d694b57db7ee48f7ea09
heisler <heisler.stlano@gmail.com>
2021-08-25 09:29:47 +0100

    remove avatar attachment

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,8 +3,8 @@ class Blog < ApplicationRecord
 	  validates :titulo, presence: true,
                     length: { minimum: 5 }
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+#	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+#	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 	  has_rich_text :texto
           def self.search(search)
 where("titulo LIKE ?", "%#{search}%")
