3b284a7dd089f89fac104ae0a03117a1eb168cec
heisler <heisler.stlano@gmail.com>
2021-10-05 13:15:19 +0100

    added some seeds and a new link  to dns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,78 @@
 # coding: utf-8
 
-
-40.times do
-  blogs = Blog.create(titulo: "familia", texto: "está é a minha familia", image_url: "https://static01.nyt.com/images/2020/04/15/business/15Techfix-illo/15Techfix-illo-jumbo.gif?quality=90&auto=webp")
-   
-end
+#1
 
 
+# alterar " characher necessario 
+40.times do 
 
-40.times do
+blogs = Blog.create(titulo: " Trabalhos da Treta: Uma Teoria (2018)", texto: "A definição é primeiro uma.
 
-  blogs = Blog.create(titulo: "Musica", texto: "isto é lofi music", image_url: "https://i0.wp.com/historiadorgeek.com.br/wp-content/uploads/2020/08/Conhe%C3%A7a-o-Brasil-Lofi-Hip-Hop.jpg?w=715&ssl=1")
+Um trabalho é uma treta se a pessoa que o faz acredita assim, porque #é seguro assumir que o trabalhador sabe melhor# e porque de outra forma seria demasiado complexo para avaliar.
 
-end
+Algo que parece útil para um estranho pode não parecer assim para outro.
+
+Graeber prevê uma definição de trabalho do conceito: #um trabalho de merda é uma forma de emprego remunerado que é tão completamente inútil, desnecessário ou pernicioso que nem mesmo o trabalhador não pode justificar a sua existência, mesmo que, no âmbito das condições de trabalho, o trabalhador se sinta obrigado a fingir que não é esse o caso#. 
+
+Portanto, um trabalho de merda envolve necessariamente um certo grau de pretensão e fraude, um fosso entre a realidade e as aparências.
+
+O autor insiste que um trabalho de merda não é o mesmo que um #trabalho de merda#, que pode ser definido como um trabalho que não paga bem e não envolve boas condições de trabalho.
+
+Muitas vezes os dois são opostos, como quanto mais útil o trabalho tende a ser, mais o trabalhador é mal tratado: por exemplo, as senhoras de limpeza são mal tratadas (as suas são #trabalhos de merda#), mas extremamente úteis, enquanto os #Especialistas em Marketing De Chumbo# são bem tratados, mas não são úteis (os deles são #trabalhos de merda#).
+
+Os flunkies existem principalmente para fazer alguém parecer ou sentir-se importante.
+
+Por exemplo, os rececionistas (que muitas vezes não têm nada para fazer) estão lá para sinalizar a importância de uma empresa .
+
+Os goons são #necessários# porque o outro lado também os tem. 
+
+Fazem coisas para promover os interesses daqueles que os empregam, mas o efeito global do seu trabalho pode ser considerado prejudicial para a humanidade no seu conjunto.
+
+Os lobistas e as pessoas de relações públicas enquadram-se nessa categoria. 
+
+Os goons sofrem muitas vezes porque têm de ser manipuladores, agressivos ou desonestos.
+
+Os sapadores são funcionários cujos postos de trabalho só existem #devido a uma falha ou falha nas organizações; que estão lá para resolver um problema que não deveria existir. 
+
+Graeber ilustra-o dando o exemplo de um empregador que tem uma fuga no telhado, mas em vez de consertá-lo, colocará um balde debaixo da fuga e contratará alguém para despejar regularmente a água.
+
+Os tapers de conduta estão muitas vezes zangados por fazerem trabalho de merda.
+
+Os box tickers são contratados para permitir que uma organização possa afirmar que está a fazer algo que realmente não está a fazer. 
+
+Por exemplo, auditores e consultores são muitas vezes box tickers: escrevem #relatórios de merda# para fazer uma organização parecer boa. 
+
+Há muitos postos de trabalho administrativos e bancários que também se enquadram nessa categoria;
+
+Os taskmasters podem ser divididos em dois grupos: o primeiro inclui aqueles que atribuem trabalho a outros e são superiores desnecessários; o segundo grupo é, na verdade, mais prejudicial porque criam tarefas de treta para outros fazerem e gerarem tretas extra. 
+
+Taskmasters podem secretamente achar os seus trabalhos inúteis, mas eles são menos propensos a admiti-lo . 
+
+A maioria dos #trabalhos de merda# são uma combinação de várias destas categorias.
+
+Também os trabalhos não-tretas podem ter elementos de treta incorporados neles.
+
+Nos trabalhos não-tretas, cada vez mais se desperdiça tempo em tarefas de merda: marcar caixas, justificar o seu trabalho, enviar e-mails a superiores e colegas. 
+
+Tanto quanto metade de qualquer trabalho pode ser assim #tretas#.
+
+Geralmente, consideramos o número de postos de trabalho antes da sua qualidade e utilidade.
+
+As nossas estatísticas e métricas não medem tretas.
+
+A nossa política pública ignora-a.
+
+ Na verdade, as nossas culturas ainda dependem de #uma tradição teológica de valorizar o trabalho como um dever sagrado#.
+
+O poder de classe e a lealdade de classe têm muito a ver com o facto de, quando se trata de emprego, o valor social e o salário estarem inversamente correlacionados. 
+
+Muitas pessoas reconhecem a relação inversa e também sentem que é assim que deve ser.
+
+Como os antigos estoicos, acreditam que a virtude deve ser a sua própria recompensa.
+
+Estes argumentos têm sido muitas vezes feitos sobre os professores: #não se gostaria que as pessoas motivadas principalmente pela ganância ensinassem crianças#.
+", image_url: "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F37994660-52a1-11e8-9871-a6b7d2d44e9e.jpg?crop=2667%2C1500%2C0%2C0")
+
+ end
+                   
+
