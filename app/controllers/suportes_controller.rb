93e5a7e29f221d001aef9f908dcedd1498313f45
heisler <heisler.stlano@gmail.com>
2022-05-25 08:52:37 +0100

    added cname feature

--- app/controllers/suportes_controller.rb
+++ app/controllers/suportes_controller.rb
@@ -13,15 +13,21 @@ def index
   @output = []
   @queryoutput = []
   @queryoutputmx = []
+  @queryoutputcn = []
   submitted_hostname = params[:hostname]
+
+
   if submitted_hostname
     @hostname = submitted_hostname
      
     begin
     resolver = Resolver.new(nameserver: @hostname)
+    rec = Recursor.new
+    ret = rec.query(@hostname, "CNAME")
+    @cnm = ret.answer
 
- 
-   
+
+
     @output = resolver.single_resolvers
        # working on query
     @mxrecord = "MX"
@@ -31,14 +37,16 @@ def index
     
     queryresolver = res.query(@hostname , @nsrecord )
 
+
    rescue   Dnsruby::NXDomain
       @nohost = "Can't find host | Certifique que o host encontra-se disponivel"
    
    else
-           queryresolvermx = res.query(@hostname,   @mxrecord )
-
+    
+    queryresolvermx = res.query(@hostname,   @mxrecord )
       @queryoutput =   queryresolver.answer
-          @queryoutputmx = queryresolvermx.answer
+      @queryoutputmx = queryresolvermx.answer
+    
     end
 
     
