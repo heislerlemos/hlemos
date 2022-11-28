6b3d286fe4cacf2d920d0898fbcbbbd2961e89e1
heislerlemos <heisler.stlamo@gmail.com>
2022-11-28 21:08:12 +0100

    adding test on cappybera

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -4,31 +4,45 @@ require 'spec_helper'
 require 'capybara/rspec'
 require "selenium-webdriver"
 
-RSpec.describe 'Hello world', type: :system do
+RSpec.describe 'Cai admin access tests', type: :system do
 
-  describe 'testing  dns ' do
+
+
+  describe 'test to check if will show up the message ' do
     it ' it should appear sever name ip ' do
-
       #visit blog_index_path
       visit '/blogs'
       visit '/suportes'
 
-
+      # this adds the  new dns name 
       fill_in 'hostname', with: 'facebook.com'
       
       click_button 'commit'
-
-
       
+      # this adds the  new dns name 
+      fill_in 'hostname', with: 'google '
+      click_button 'commit'         
 
-      fill_in 'hostname', with: 'google    '
-      
-       click_button 'commit'
-         
        #expect(find('.hero-body .title')).to have_content("Can't find host  Certifique que o host encontra-se disponivel !")
-        expect(page).to have_css(".title", text: "heisler")
+       # what supost to happen for the test pass need's to have the text in cause
+       expect(page).to have_css(".title", text: "Can't find host | Certifique que o host encontra-se disponivel !")
 
     end
-
   end
+
+  
+
+  describe 'access administration panel', js: true do
+    
+    it "tries to access the panel administration with the tickets"  , driver: :apparition do  
+     
+
+        visit "http://cai:cai2021luanda@0.0.0.0:3000/subscribers/show"
+          click_button "commit"
+          visit "/welcomes/index"
+            expect(page).to have_css(".slogan", text:  "Cai\nna nossa\ninovação")
+    end
+    
+  end
+
 end
