c12a5408eabd28b9961b45167b38178bcb5435f4
heislerlemos <heisler.stlamo@gmail.com>
2023-01-03 13:36:49 +0100

    making some changes on rspec

--- spec/cai_spec.rb
+++ spec/cai_spec.rb
@@ -20,6 +20,20 @@ RSpec.describe 'Testing all acess from cai page', type: :system do
   end
 
 
+describe 'will fill the form on the subscribers' do 
+        it 'should fill all the form ' do 
+          visit '/subscribers'
+          fill_in 'subscriber[name]', with: "Heisler Jorge De Lemos Stlano"
+          fill_in 'subscriber[number]', with: "934659342"
+          fill_in 'subscriber[email]', with: "heisler.lemos@mail.com"
+          fill_in 'subscriber[subject]', with: "testing systems "
+          find('trix-editor').click.set('Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.')
+          select "Redes", :from => "subscriber[source]"
+          click_button 'commit'
+
+        end 
+end 
+
   describe 'test to check if will show up the message ' do
     it ' it should appear sever name ip ' do
       visit '/blogs'
