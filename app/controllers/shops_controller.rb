59623917d17a44db56096ac10e6a74eb9f9e874b
heislerlemos <heisler.stlano@gmail.com>
2025-05-05 13:40:56 +0100

    testing the authentication token

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,7 @@
 class ShopsController < ApplicationController
+ skip_before_action :verify_authenticity_token
+
+
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
