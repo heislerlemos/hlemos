524f57d4011de728841e0723579ef4a920a58ab7
heislerlemos <heisler.stlano@gmail.com>
2025-05-10 09:04:51 +0100

    added display list

--- app/controllers/binarys_controller.rb
+++ app/controllers/binarys_controller.rb
@@ -1,9 +1,9 @@
 class BinarysController < ApplicationController
   def index
-   
-   @hostname = "127.0.0.1".downcase
+
+    @hostname = "127.0.0.1".downcase
     submitted_hostname = params[:hostname]
-    
+
     if submitted_hostname 
       @hostname = submitted_hostname
       ip = IPAddress::IPv4.new @hostname
@@ -13,17 +13,20 @@ class BinarysController < ApplicationController
       end
 
       ipsubnet   = Ip.new @hostname
-      ipsubstore = []
+      stored = [] 
       @bits = bits
       @mask = ip.netmask
       @ipv6 = ip.to_ipv6
-      
+
+
+
       32.downto(1) do |i|
-        print "   " + ipsubnet.cidr(i)
-        ipsubstore = ipsubnet.cidr(i)
-       end
-         @ipsub = ipsubstore     
-      
+        print " " + ipsubnet.cidr(i)
+        stored.append(ipsubnet.cidr(i))
+      end
+
+      @ipsub =  stored 
+
     end
 
   end
