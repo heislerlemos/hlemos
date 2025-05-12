084ace2a5603bab9887e2f83e42306967fc8d5a7
heislerlemos <heisler.stlano@gmail.com>
2025-05-12 15:08:10 +0100

    fixed video layout

--- config/application.rb
+++ config/application.rb
@@ -14,13 +14,13 @@ Bundler.require(*Rails.groups)
 
 module Hlemos
   class Application < Rails::Application
-        #config.autoload_paths += %W(#{config.root}/lib)
-#config.autoload_paths << Rails.root.join('lib')
- #config.eager_load_paths << Rails.root.join('lib')
-       config.eager_load_paths << config.root / 'lib'
+    #config.autoload_paths += %W(#{config.root}/lib)
+    #config.autoload_paths << Rails.root.join('lib')
+    #config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << config.root / 'lib'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
