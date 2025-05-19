f7fb24517bd13969f8360288033267b56e3be446
heislerlemos <heisler.stlano@gmail.com>
2025-05-19 08:46:32 +0100

    added exception on speedtest

--- app/controllers/speeds_controller.rb
+++ app/controllers/speeds_controller.rb
@@ -1,26 +1,26 @@
 class SpeedsController < ApplicationController
-require 'speedtest/speedtest'
+  require 'speedtest/speedtest'
 
+  def index
+    submit = params[:empty]
+    if submit
 
-def index
-submit = params[:empty]
-if submit
+      loading = "Estamos a verificar a connexao de speedtest por favor aguarde um momento"
+      begin     
+        stored = []
+        speed = Speedtest::SpeedTest.new(ARGV)
+        speed.run.each do |x,y|
+          puts "#{x} =>  #{y}"
+          value = "#{x} =>  #{y}"
+          stored.append(value)
+        end
 
-  loading = "Estamos a verificar a connexao de speedtest por favor aguarde um momento"
-    
-  stored = []
-  speed = Speedtest::SpeedTest.new(ARGV)
-  speed.run.each do |x,y|
-    puts "#{x} =>  #{y}"
-    value = "#{x} =>  #{y}"
-    stored.append(value)
+        @output = stored
+      rescue Net::HTTP::Persistent::Error 
+        @erro =  'Erro de por favor volte a confirmar se o problema persistir verifique a configuração da  sua rede'
+      end
+    end
   end
-  
-@output = stored
-
-
-end
-end
 
 end
 
