72f68cd7cd052dcd55b302fa34a443a8c525e6d1
heislerlemos <heisler.stlano@gmail.com>
2025-06-23 17:07:22 +0100

    fixing layout shop

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,5 @@
 //= link index.mp4
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link turbolinks.js
+//= require turbolinks
--- app/controllers/aulas_controller.rb
+++ app/controllers/aulas_controller.rb
@@ -1,11 +1,7 @@
 class AulasController < ApplicationController
   include AulasHelper
-
+  
   def index
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
-
-
-end
-
-
+  end
 end
