2a69656646eaca3a604079923194465eddcbae2b
heislerlemos <heisler.stlano@gmail.com>
2026-06-26 16:37:07 +0100

    fixing views on cardbox of shop

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 # fixing order error 
 
- Order.create(id: 1)
+Order.create(id: 1)
 
 
 
@@ -26,7 +26,7 @@ Os flunkies existem principalmente para fazer alguém parecer ou sentir-se impor
 
 Por exemplo, os rececionistas (que muitas vezes não têm nada para fazer) estão lá para sinalizar a importância de uma empresa .
 Estes argumentos têm sido muitas vezes feitos sobre os professores: #não se gostaria que as pessoas motivadas principalmente pela ganância ensinassem crianças#.
-                    ",  image_url: "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F37994660-52a1-11e8-9871-a6b7d2d44e9e.jpg?crop=2667%2C1500%2C0%2C0")
+            ",  image_url: "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F37994660-52a1-11e8-9871-a6b7d2d44e9e.jpg?crop=2667%2C1500%2C0%2C0")
 
 
 
@@ -35,4 +35,112 @@ Product.create(titulo: "RADWIN 2000",   preco: 100,   imagelink: "https://amboya
 Product.create(titulo: "AULA S99 Gaming Keyboard", preco: 26, imagelink: "https://m.media-amazon.com/images/I/61r9aziJGFL._AC_SX300_SY300_QL70_FMwebp_.jpg")
 Product.create(titulo: "Lenovo ThinkPad T14 G6 ", preco: 1198, imagelink: "https://m.media-amazon.com/images/I/71raaxw3cML._AC_SX466_.jpg")
 Product.create(titulo: "Garmin Forerunner 165,", preco: 199, imagelink: "https://m.media-amazon.com/images/I/61m+fKy7wzL._AC_SL1500_.jpg")
+Product.create(titulo: "Cisco Catalyst 9300 Switch", preco: 3500, imagelink: "https://info.pivitglobal.com/hubfs/catalyst-9300.jpeg")
+Product.create(titulo: "Fortinet FortiGate 60F", preco: 850, imagelink: "https://amboya.com/wp-content/uploads/2023/04/fortigate-60f.png")
+Product.create(titulo: "Ubiquiti UniFi U6 Pro AP", preco: 150, imagelink: "https://m.media-amazon.com/images/I/51Z-N2vDLxL._AC_SX466_.jpg")
+Product.create(titulo: "MikroTik Cloud Core Router 2004", preco: 450, imagelink: "https://m.media-amazon.com/images/I/61bK2B3N9pL._AC_SL1500_.jpg")
+Product.create(titulo: "Logitech MX Master 3S Mouse", preco: 99, imagelink: "https://m.media-amazon.com/images/I/614-I97S7cL._AC_SX300_SY300_QL70_FMwebp_.jpg")
 
+Product.create(titulo: "Dell Latitude 5440 Laptop", preco: 1050, imagelink: "https://m.media-amazon.com/images/I/71c5w3mCML._AC_SX466_.jpg")
+Product.create(titulo: "HP ProBook 450 G10", preco: 950, imagelink: "https://m.media-amazon.com/images/I/71vR4axw3cM._AC_SL1500_.jpg")
+Product.create(titulo: "Garmin Forerunner 265", preco: 449, imagelink: "https://m.media-amazon.com/images/I/61m+fKy7wzL._AC_SL1500_.jpg")
+Product.create(titulo: "Apple Watch Series 9", preco: 399, imagelink: "https://m.media-amazon.com/images/I/71X8v4aWkKL._AC_SL1500_.jpg")
+Product.create(titulo: "Samsung Odyssey G5 Monitor 27\"", preco: 279, imagelink: "https://m.media-amazon.com/images/I/81TjL-q0SBL._AC_SX466_.jpg")
+
+Product.create(titulo: "Keychron K2 Mechanical Keyboard", preco: 89, imagelink: "https://m.media-amazon.com/images/I/71r9aziJGFL._AC_SX300_SY300_QL70_FMwebp_.jpg")
+Product.create(titulo: "Raspberry Pi 5 8GB Starter Kit", preco: 120, imagelink: "https://m.media-amazon.com/images/I/81xG0B-88xL._AC_SX466_.jpg")
+Product.create(titulo: "Synology DiskStation DS224+", preco: 299, imagelink: "https://m.media-amazon.com/images/I/61y8B1G9VKL._AC_SL1100_.jpg")
+Product.create(titulo: "QNAP TS-464 NAS Storage", preco: 549, imagelink: "https://m.media-amazon.com/images/I/61Nf7BxG3KL._AC_SL1200_.jpg")
+Product.create(titulo: "Anker Prime 20000mAh Power Bank", preco: 130, imagelink: "https://m.media-amazon.com/images/I/61pB9V3N9pL._AC_SL1500_.jpg")
+
+Product.create(titulo: "Sony WH-1000XM5 Headphones", preco: 348, imagelink: "https://m.media-amazon.com/images/I/51aB43N9pL._AC_SL1200_.jpg")
+Product.create(titulo: "Elgato Stream Deck MK.2", preco: 149, imagelink: "https://m.media-amazon.com/images/I/61yXfBxG3KL._AC_SL1500_.jpg")
+Product.create(titulo: "Crucial T700 2TB PCIe 5.0 NVMe SSD", preco: 260, imagelink: "https://m.media-amazon.com/images/I/71v1BxG3KL._AC_SL1500_.jpg")
+Product.create(titulo: "Corsair Vengeance DDR5 32GB RAM", preco: 115, imagelink: "https://m.media-amazon.com/images/I/61N7BxG3KLL._AC_SL1500_.jpg")
+Product.create(titulo: "ASUS ROG Strix GeForce RTX 4070", preco: 699, imagelink: "https://m.media-amazon.com/images/I/81Nf7BxG3KL._AC_SL1500_.jpg")
+
+
+Subscriber.create(name: "Carlos Silva", number: 923000111, email: "carlos.silva@email.com",
+                  issue: "Não consigo conectar à VPN da empresa desde hoje de manhã. Dá erro de timeout." , 
+                  subject: "Falha na VPN", source: "Redes")
+
+# Tickets dos users-
+Subscriber.create(name: "Carlos Silva", number: 923000111, email: "carlos.silva@email.com",
+                  issue: "Não consigo conectar à VPN da empresa desde hoje de manhã. Dá erro de timeout.",
+                  subject: "Falha na VPN", source: "Redes")
+
+Subscriber.create(name: "Ana Jorge", number: 931444222, email: "ana.jorge@empresa.ao",
+                  issue: "A velocidade contratada de 50 Mbps está batendo apenas 5 Mbps no cabo.",
+                  subject: "Lentidão na Internet", source: "Redes")
+
+Subscriber.create(name: "Mateus Neto", number: 912555333, email: "mateus@dev.co.ao",
+                  issue: "Após o deploy do script PHP, a página principal está retornando Erro Interno do Servidor.",
+                  subject: "Erro 500 no Web Server", source: "Aplicação")
+
+Subscriber.create(name: "Cláudia Dias", number: 945777888, email: "claudia.dias@loja.com",
+                  issue: "O router principal está com a luz LOS vermelha piscando. Estamos sem comunicação.",
+                  subject: "Queda de Link Fibra", source: "Redes")
+
+Subscriber.create(name: "Ricardo Santos", number: 924999111, email: "ricardo.s.tech@gmail.com",
+                  issue: "Preciso da liberação da porta 443 e 8080 para um novo servidor interno de homologação.",
+                  subject: "Bloqueio de Firewall", source: "Servidor")
+
+Subscriber.create(name: "Sandra Gomes", number: 933222333, email: "sandra.gomes@outlook.com",
+                  issue: "Solicito a alteração dos apontamentos MX do nosso domínio para migração do e-mail.",
+                  subject: "Configuração de DNS", source: "Redes")
+
+Subscriber.create(name: "João Paulo", number: 917888999, email: "jp.sistemas@net.ao",
+                  issue: "Estamos enfrentando alta latência e perda de pacotes (~15%) nos testes de ping para o gateway.",
+                  subject: "Perda de Pacotes", source: "Redes")
+
+Subscriber.create(name: "Teresa Bento", number: 926111222, email: "teresa.bento@banco.ao",
+                  issue: "Esquecemos a senha de admin do switch de borda do segundo andar. Necessário password recovery.",
+                  subject: "Reset de Senha Switch", source: "Redes")
+
+Subscriber.create(name: "Adilson Sousa", number: 941333444, email: "adilson.sousa@tech.ao",
+                  issue: "O job de backup agendado para a madrugada falhou com erro de falta de espaço no storage.",
+                  subject: "Backup Nuvem Falhou", source: "Servidor")
+
+Subscriber.create(name: "Márcia Lima", number: 932777666, email: "marcia.lima@comercial.com",
+                  issue: "Necessitamos da configuração de um novo Access Point na sala de reuniões principal.",
+                  subject: "Instalação de AP Wi-Fi", source: "Redes")
+
+Subscriber.create(name: "Bruno Costa", number: 919444555, email: "bruno.c@startup.ao",
+                  issue: "O formulário do site parou de enviar os leads para o CRM via API REST.",
+                  subject: "Integração de API", source: "Aplicação")
+
+Subscriber.create(name: "Patrícia Cruz", number: 928666777, email: "p.cruz@educa.ao",
+                  issue: "O site institucional está mostrando aviso de conexão não segura. É preciso renovar o SSL.",
+                  subject: "Certificado SSL Expirado", source: "Servidor")
+
+Subscriber.create(name: "Fernando Rocha", number: 935888111, email: "fernando.rocha@log.ao",
+                  issue: "Preciso criar uma nova VLAN (ID 20) para isolar o tráfego do setor de Recursos Humanos.",
+                  subject: "Configuração de VLAN", source: "Redes")
+
+Subscriber.create(name: "Isabel Martins", number: 911222333, email: "isabel.m@media.co.ao",
+                  issue: "As chamadas telefónicas via IP estão cortando muito e com eco constante.",
+                  subject: "Instabilidade VoIP", source: "Redes")
+
+Subscriber.create(name: "Valter Nunes", number: 927555444, email: "valter.nunes@loja.ao",
+                  issue: "O site apresenta \"Error establishing a database connection\" intermitentemente.",
+                  subject: "Erro de Banco de Dados", source: "Servidor")
+
+Subscriber.create(name: "António Sousa", number: 923111000, email: "antonio.sousa@rh.ao",
+                  issue: "O computador do secretariado não liga. Já verifiquei os cabos e a tomada tem energia.",
+                  subject: "Desktop Não Liga", source: "Desktop")
+
+Subscriber.create(name: "Catarina Manuel", number: 942888222, email: "catarina.m@vendas.ao",
+                  issue: "Não consigo imprimir os relatórios na impressora de rede. O documento fica preso na fila.",
+                  subject: "Falha de Impressora", source: "Desktop")
+
+Subscriber.create(name: "Pedro Lima", number: 914333555, email: "pedro.lima@producao.ao",
+                  issue: "O ERP da empresa está extremamente lento para processar o encerramento do mês.",
+                  subject: "Lentidão no ERP", source: "Aplicação")
+
+Subscriber.create(name: "Daniela Vaz", number: 936777111, email: "daniela.vaz@financas.ao",
+                  issue: "Recebemos um alerta de ataque de força bruta IP externo tentando autenticar via SSH no servidor SSH.",
+                  subject: "Alerta de Segurança", source: "Servidor")
+
+Subscriber.create(name: "Miguel Santos", number: 925000444, email: "miguel.santos@suporte.ao",
+                  issue: "O monitor principal está piscando e apresentando linhas horizontais verdes na tela.",
+                  subject: "Problema no Monitor", source: "Desktop")
