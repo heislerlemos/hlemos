f6335dd4d2b5232153b207710f2301cfad650afc
heislerlemos <heisler.stlano@gmail.com>
2026-07-10 21:47:10 +0100

    removing one javascript ....

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link index.mp4
-//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link turbolinks.js
 //= require turbolinks
