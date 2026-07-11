5baf45f8b0f9ac0463112a2ef87b1a7d3e59fdb4
heislerlemos <heisler.stlano@gmail.com>
2026-07-11 16:11:26 +0100

    fixing virus controller typo

--- app/controllers/virus_controller.rb
+++ app/controllers/virus_controller.rb
@@ -1,8 +1,7 @@
-=begin
 
 class VirusController < ApplicationController
   def index
-  	   response = HTTParty.get('http://api.thevirustracker.com')
+    response = HTTParty.get('http://api.thevirustracker.com')
 
     puts "At the moment covid server is #{response.code}"
     puts "At the moment covid server is #{response.body}"
@@ -11,59 +10,59 @@ class VirusController < ApplicationController
 
 
 
-@nostatus = "no info covid at the moment"
+      @nostatus = "no info covid at the moment"
 
-else if  response.code  == 200 ||  response.code  == 404 ||  response.code  == 401 ||  response.code  == 500
+    else if  response.code  == 200 ||  response.code  == 404 ||  response.code  == 401 ||  response.code  == 500
 
-    @nostatus = "Não existe informação de momento "
+      @nostatus = "Não existe informação de momento "
 
-else
+    else
 
-         statistics = Covid19::GlobalStatistics.retrieve
-         @totalcases = JSON.parse(statistics)["results"].first["total_cases"]
-         @totalrecoverd = JSON.parse(statistics)["results"].first["total_recovered"]
-         @totaldeaths = JSON.parse(statistics)["results"].first["total_deaths"]
-     # Nacional
-         angolaestatica = Covid19::GlobalStatistics.by(country: 'ao')
-         @totalcasesangola  = JSON.parse(angolaestatica)["countrydata"].first["total_cases"]
-         @totalrecoverdangola = JSON.parse(angolaestatica)["countrydata"].first["total_recovered"]
-         @totaldeathsangola =  JSON.parse(angolaestatica)["countrydata"].first["total_deaths"]
-       @news = Covid19::News.retrieve
-         puts statistics.class
+      statistics = Covid19::GlobalStatistics.retrieve
+      @totalcases = JSON.parse(statistics)["results"].first["total_cases"]
+      @totalrecoverd = JSON.parse(statistics)["results"].first["total_recovered"]
+      @totaldeaths = JSON.parse(statistics)["results"].first["total_deaths"]
+      # Nacional
+      angolaestatica = Covid19::GlobalStatistics.by(country: 'ao')
+      @totalcasesangola  = JSON.parse(angolaestatica)["countrydata"].first["total_cases"]
+      @totalrecoverdangola = JSON.parse(angolaestatica)["countrydata"].first["total_recovered"]
+      @totaldeathsangola =  JSON.parse(angolaestatica)["countrydata"].first["total_deaths"]
+      @news = Covid19::News.retrieve
+      puts statistics.class
 
-         portugalcovid =  Covid19::GlobalStatistics.by(country: 'pt')
-         @portugalestado = JSON.parse(portugalcovid)["countrydata"].first["total_cases"]
-         @portugalmortes = JSON.parse(portugalcovid)["countrydata"].first["total_deaths"]
-         @portugarecuperados = JSON.parse(portugalcovid)["countrydata"].first["total_recovered"]
-
-
-
-         brazilcovid =  Covid19::GlobalStatistics.by(country: 'br')
-         @brazilestado = JSON.parse( brazilcovid )["countrydata"].first["total_cases"]
-         @brazilmorte = JSON.parse( brazilcovid )["countrydata"].first["total_deaths"]
-         @brazilrecuperados = JSON.parse( brazilcovid )["countrydata"].first["total_recovered"]
+      portugalcovid =  Covid19::GlobalStatistics.by(country: 'pt')
+      @portugalestado = JSON.parse(portugalcovid)["countrydata"].first["total_cases"]
+      @portugalmortes = JSON.parse(portugalcovid)["countrydata"].first["total_deaths"]
+      @portugarecuperados = JSON.parse(portugalcovid)["countrydata"].first["total_recovered"]
 
 
-    # Para verificar a info
-    puts "---------------------------------"
-    puts "---------------------------------"
-        puts "valores do mundo #{statistics}"
-        puts "---------------------------------"
-        puts "---------------------------------"
 
-        puts "valores de angola #{angolaestatica}"
-        puts "---------------------------------"
-        puts "---------------------------------"
+      brazilcovid =  Covid19::GlobalStatistics.by(country: 'br')
+      @brazilestado = JSON.parse( brazilcovid )["countrydata"].first["total_cases"]
+      @brazilmorte = JSON.parse( brazilcovid )["countrydata"].first["total_deaths"]
+      @brazilrecuperados = JSON.parse( brazilcovid )["countrydata"].first["total_recovered"]
 
-        puts "valores de portugal #{portugalcovid }"
-        puts "---------------------------------"
-        puts "---------------------------------"
 
-        puts "valores do brazil #{brazilcovid}"
-        puts "---------------------------------"
-        puts "---------------------------------"
+      # Para verificar a info
+      puts "---------------------------------"
+      puts "---------------------------------"
+      puts "valores do mundo #{statistics}"
+      puts "---------------------------------"
+      puts "---------------------------------"
 
-  end
+      puts "valores de angola #{angolaestatica}"
+      puts "---------------------------------"
+      puts "---------------------------------"
+
+      puts "valores de portugal #{portugalcovid }"
+      puts "---------------------------------"
+      puts "---------------------------------"
+
+      puts "valores do brazil #{brazilcovid}"
+      puts "---------------------------------"
+      puts "---------------------------------"
+
+    end
   end
 end
-=end
+end
