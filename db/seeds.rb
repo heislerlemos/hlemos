4ffc5b1c1cf152888ac4c3c7157fd5eff5667831
heislerlemos <heisler.stlano@gmail.com>
2026-07-22 14:15:06 +0100

    adding seeds on comments and blog

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,25 @@
 # coding: utf-8
 
-# fixing order error 
-
+# fixing order error when create new database
 Order.create(id: 1)
 
 
 
-# alterar " characher necessario 
-
-Blog.create(titulo: " Trabalhos da Treta: Uma Teoria (2018)", texto: "A definição é primeiro uma.
-
-Um trabalho é uma treta se a pessoa que o faz acredita assim, porque #é seguro assumir que o trabalhador sabe melhor# e porque de outra forma seria demasiado complexo para avaliar.
-
-Algo que parece útil para um estranho pode não parecer assim para outro.
-
-Graeber prevê uma definição de trabalho do conceito: #um trabalho de merda é uma forma de emprego remunerado que é tão completamente inútil, desnecessário ou pernicioso que nem mesmo o trabalhador não pode justificar a sua existência, mesmo que, no âmbito das condições de trabalho, o trabalhador se sinta obrigado a fingir que não é esse o caso#. 
-
-Portanto, um trabalho de merda envolve necessariamente um certo grau de pretensão e fraude, um fosso entre a realidade e as aparências.
-
-O autor insiste que um trabalho de merda não é o mesmo que um #trabalho de merda#, que pode ser definido como um trabalho que não paga bem e não envolve boas condições de trabalho.
-
-Muitas vezes os dois são opostos, como quanto mais útil o trabalho tende a ser, mais o trabalhador é mal tratado: por exemplo, as senhoras de limpeza são mal tratadas (as suas são #trabalhos de merda#), mas extremamente úteis, enquanto os #Especialistas em Marketing De Chumbo# são bem tratados, mas não são úteis (os deles são #trabalhos de merda#).
-
-Os flunkies existem principalmente para fazer alguém parecer ou sentir-se importante.
-
-Por exemplo, os rececionistas (que muitas vezes não têm nada para fazer) estão lá para sinalizar a importância de uma empresa .
-Estes argumentos têm sido muitas vezes feitos sobre os professores: #não se gostaria que as pessoas motivadas principalmente pela ganância ensinassem crianças#.
-            ",  image_url: "https://www.thetimes.co.uk/imageserver/image/methode%2Fsundaytimes%2Fprod%2Fweb%2Fbin%2F37994660-52a1-11e8-9871-a6b7d2d44e9e.jpg?crop=2667%2C1500%2C0%2C0")
+# Blogs 
+Blog.create(titulo: "Microsoft just shipped its own general-purpose Linux distro: Azure Linux 4.0", texto: "Microsoft released Azure Linux 4, a Fedora based general purpose server distro available as an Azure VM and under WSL. Interesting to see Microsoft shipping its own Linux distro after years of mostly hosting others.",  image_url: "https://cdn.shortpixel.ai/spai/q_lossy+ret_img+to_auto/linuxiac.com/wp-content/uploads/2026/06/azure-linux-4-released-1024x576.jpg")
 
 
 
+# Commentarios
+Comentario.create(  comentariador: "JoJoModding", corpo: "
+Microsoft released Azure Linux 4, a Fedora based general purpose server distro available as an Azure VM and under WSL. Interesting to see Microsoft shipping its own Linux distro after years of mostly hosting others.", blog_id: 1)
+Comentario.create(  comentariador: "Jumpy-Dinner-5001", corpo: "
+As the name suggests: It's nothing new. It's similar to Amazon Linux and simply a Fedora that is optimized for cloud use in AWS or Azure.", blog_id: 1)
+
+
+
+
+# Produtos
 Product.create(titulo: "ASR 1000 Series Routers",   preco: 500,   imagelink: "https://info.pivitglobal.com/hubfs/blob-4.jpeg")
 Product.create(titulo: "RADWIN 2000",   preco: 100,   imagelink: "https://amboya.com/wp-content/uploads/2023/04/Radwin-Point-To-Point.png")
 Product.create(titulo: "AULA S99 Gaming Keyboard", preco: 26, imagelink: "https://m.media-amazon.com/images/I/61r9aziJGFL._AC_SX300_SY300_QL70_FMwebp_.jpg")
